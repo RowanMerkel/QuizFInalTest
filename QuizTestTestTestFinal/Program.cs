e375b54411045d1e7582ca55b8114835a54ace86
Rowan Merkel <14103281@student.hhs.nl>
2020-01-05 00:05:07 +0100

    FirstCommit

--- QuizTestTestTestFinal/Program.cs
+++ QuizTestTestTestFinal/Program.cs
@@ -6,7 +6,7 @@ namespace QuizTestTestTestFinal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Git is werkende.");
         }
     }
 }
